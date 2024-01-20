818f9fc027d78551baf0a5e5c9ca310b634034c5
Raditya Harya <contact@radityaharya.com>
2024-01-20 16:46:32 +0700

    add workerid to instrumentation worker registration

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -2,11 +2,15 @@ import { env } from "~/env";
 import { Runner } from "~/lib/workflow/Workflow";
 import { type Workflow } from "~/lib/workflow/types";
 import { getAccessTokenFromUserId } from "~/server/db/helper";
+import { v4 as uuid } from "uuid";
+
+const WORKER_ID = process.env.WORKER_ID ?? `instrumentation-${uuid()}`;
 
 export const register = async () => {
   //This if statement is important, read here: https://nextjs.org/docs/app/building-your-application/optimizing/instrumentation
   if (process.env.NEXT_RUNTIME === "nodejs" && !process.env.NO_WORKER) {
     console.log("Registering worker");
+    console.log("Worker ID", WORKER_ID);
     const { Worker } = await import("bullmq");
     const Redis = (await import("ioredis")).default;
     const updateWorkflowJob = (await import("~/app/api/workflow/workflowQueue")).updateWorkflowJob;
@@ -18,7 +22,7 @@ export const register = async () => {
       "workflowQueue",
       async (job) => {
         const data = job?.data;
-        await updateWorkflowJob(job.id!, "active");
+        await updateWorkflowJob(job.id!, "active", WORKER_ID);
         if (!data) {
           throw new Error("No data found in job");
         }
@@ -33,12 +37,12 @@ export const register = async () => {
         try{
           res = await runner.runWorkflow(workflow);
         } catch(e){
-          await updateWorkflowJob(job.id!, "failed");
+          await updateWorkflowJob(job.id!, "failed", WORKER_ID);
           console.error("Error running workflow", e);
           throw e;
         }
         // console.log("Workflow executed successfully", res);
-        await updateWorkflowJob(job.id!, "completed");
+        await updateWorkflowJob(job.id!, "completed", WORKER_ID);
         // console.log("Updated job", updated);
         // console.log(data);
         return res;
