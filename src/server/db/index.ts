10fe8da1b09e7fa946fa6f4e6684c6836a8f93a3
Raditya Harya <radityaharya02@gmail.com>
2024-04-30 15:14:24 +0000

    fix: development db connection

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,7 +1,26 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 
-import { env } from "~/env";
-import * as schema from "./schema";
+import { env } from "~/env.js";
+import * as schema from "./schema"; 
 
-export const db = drizzle(postgres(env.DATABASE_URL), { schema });
+import { type PostgresJsDatabase } from "drizzle-orm/postgres-js";
+
+// Fix for "sorry, too many clients already"
+declare global {
+  // eslint-disable-next-line no-var -- only var works here
+  var db: PostgresJsDatabase<typeof schema> | undefined;
+}
+
+// biome-ignore lint/suspicious/noRedeclare: <explanation>
+let db: PostgresJsDatabase<typeof schema>;
+
+if (env.NODE_ENV === "production") {
+  db = drizzle(postgres(env.DATABASE_URL), { schema });
+} else {
+  if (!global.db) global.db = drizzle(postgres(env.DATABASE_URL), { schema });
+
+  db = global.db;
+}
+
+export { db };
