b751628e1b1d6a95c348d317028d650fc8ceee7a
Raditya Harya <radityaharya02@gmail.com>
2024-05-08 12:03:54 +0000

    logout button

--- src/components/main-nav.tsx
+++ src/components/main-nav.tsx
@@ -2,17 +2,27 @@
 
 import { SystemInfo } from "@/components/SystemInfo";
 import Link, { LinkProps } from "next/link";
-import { usePathname } from "next/navigation";
-import { buttonVariants } from "~/components/ui/button";
+import { usePathname, useRouter } from "next/navigation";
+import { Button, buttonVariants } from "~/components/ui/button";
 
 import { cn } from "@/lib/utils";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 import useStore from "@/app/states/store";
 import { Session } from "next-auth";
 import { useMemo } from "react";
 import useSWR from "swr";
 import { fetcher } from "~/app/utils/fetcher";
+import { signOut } from "next-auth/react";
+import { SquareArrowOutUpRight } from "lucide-react";
 
 interface NavLinkProps extends LinkProps {
   href: string;
@@ -111,6 +121,12 @@ export function SiteNav({ className, session }: SiteNavProps) {
     fallbackData: session || undefined,
   });
   const pathname = usePathname();
+  const router = useRouter();
+
+  const handleLogout = () => {
+    signOut();
+    router.push("/");
+  };
 
   const navClass = useMemo(() => {
     let classes =
@@ -134,19 +150,50 @@ export function SiteNav({ className, session }: SiteNavProps) {
         </div>
         {sessionData?.user ? (
           <div className="flex flex-row items-center gap-4">
-            <span className="font-medium text-foreground text-sm">
-              {`${sessionData.user.name}`}
-            </span>
-            <Avatar className="h-8 w-8">
-              <AvatarImage
-                src={sessionData?.user?.image ?? ""}
-                alt={sessionData?.user?.name ?? ""}
-              />
-              <AvatarFallback className="font-medium text-sm">
-                {/* biome-ignore lint/correctness/useJsxKeyInIterable: <explanation> */}
-                {sessionData?.user?.name?.split(" ").map((n) => n[0])}
-              </AvatarFallback>
-            </Avatar>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button
+                  variant="outline"
+                  className="overflow-hidden rounded-full h-8 w-8"
+                >
+                  <Avatar className="h-8 w-8">
+                    <AvatarImage
+                      src={sessionData?.user?.image ?? ""}
+                      alt={sessionData?.user?.name ?? ""}
+                    />
+                    <AvatarFallback className="font-medium text-sm">
+                      {/* biome-ignore lint/correctness/useJsxKeyInIterable: <explanation> */}
+                      {sessionData?.user?.name?.split(" ").map((n) => n[0])}
+                    </AvatarFallback>
+                  </Avatar>
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="end">
+                <DropdownMenuLabel className="flex flex-col">
+                  <p className="font-medium text-foreground text-sm">
+                    {`${sessionData.user.name}`}
+                  </p>
+                  <p className="text-muted-foreground text-xs">
+                    {`${sessionData.user.email}`}
+                  </p>
+                </DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem asChild>
+                  <Link
+                    href="https://www.spotify.com/account/overview/"
+                    target="_blank"
+                    className="flex flex-row gap-2 items-center"
+                  >
+                    <span>Spotify Account</span>
+                    <SquareArrowOutUpRight className="size-3"/>
+                  </Link>
+                </DropdownMenuItem>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem onClick={handleLogout}>
+                  Logout
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
           </div>
         ) : (
           <div className="h-8 flex items-center">
